dca2ccba706ff7e7706a29a7d334fc1a56a31ed8
Crystallizee <benidictus.tf@yahoo.com>
2021-09-23 16:50:34 +0700

    first commit

---
+++ app/Models/Departemen.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Departemen extends Model
+{
+    protected $table = 'data_departemen';
+    protected $fillable = [
+        'nama_departemen',
+        'nama_manager',
+        'jumlah_pegawai'
+    ];
+
+}
